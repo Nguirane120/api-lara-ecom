a05184320c1b9d0747c2bf375fb7f36a7c7fe6ca
Nguirane120 <badaranguirane120@gmail.com>
2022-03-13 13:47:29 +0000

    api users

--- routes/api.php
+++ routes/api.php
@@ -23,3 +23,7 @@ Route::middleware(['auth:sanctum', 'isAdmin'])->group(function () {
         return response()->json(['message' => 'Youare in', 'status' => 200], 200);
     });
 });
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
